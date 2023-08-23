fed883dd908e37bff327af7fc40e8b53f74d2661
DucThang22 <50301049+DucThang22@users.noreply.github.com>
2023-08-23 19:01:33 +0700

    DucThang97

---
+++ app/src/main/java/com/example/hahalolofake/component/AppModule.kt
@@ -0,0 +1,18 @@
+package com.example.hahalolofake.component
+
+import com.example.hahalolofake.data.api.ApiModule
+import com.example.hahalolofake.data.repository.RepositoryModule
+import dagger.Module
+
+@Module(
+    includes = [
+        // module of Activity && ViewModel
+        ActivityModule::class,
+        ViewModelModule::class,
+
+        // module of data
+        ApiModule::class,
+        RepositoryModule::class,
+    ]
+)
+class AppModule
---
+++ app/src/main/java/com/example/hahalolofake/component/ViewModelModule.kt
@@ -0,0 +1,21 @@
+package com.example.hahalolofake.component
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.hahalolofake.component.ViewModelFactory
+import com.example.hahalolofake.component.ViewModelKey
+import com.example.hahalolofake.ui.MainActivityViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class ViewModelModule {
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainActivityViewModel::class)
+    abstract fun bindMainActivityViewModel(mainActivityViewModel: MainActivityViewModel): ViewModel
+}
